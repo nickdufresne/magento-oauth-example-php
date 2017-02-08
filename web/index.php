7954bbcdb2ce3537a4e8505fddcb08ebc6ff03dd
Nick Dufresne <nick.dufresne@pvep.com>
2017-02-08 08:43:37 -0500

    Initial oauth app

---
+++ web/index.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Example of retrieving the products list using Admin account via Magento REST API. OAuth authorization is used
+ * Preconditions:
+ * 1. Install php oauth extension
+ * 2. If you were authorized as a Customer before this step, clear browser cookies for 'yourhost'
+ * 3. Create at least one product in Magento
+ * 4. Configure resource permissions for Admin REST user for retrieving all product data for Admin
+ * 5. Create a Consumer
+ */
+// $callbackUrl is a path to your file with OAuth authentication example for the Admin user
+
+// config vars set on the heroku app 
+// heroku config:add BASE_URL=...
+$baseURL = getenv("BASE_URL");
+$consumerKey = getenv("CONSUMER_KEY");
+$consumerSecret = getenv("CONSUMER_SECRET");
+$callbackUrl = getenv("CALLBACK_URL");
+
+$requestTokenURL = $baseURL."/oauth/initiate";
+$authorizeURL = $baseURL."/stockroom/oauth_authorize";
+$accessTokenURL = $baseURL."/oauth/token";
+$apiUrl = $baseURL."/api/rest";
+
+if (isset($_GET["reset_session"])) {
+    session_destroy();
+}
+
+session_start();
+
+// helper to rerun the oauth flow with updated Magento settings
+if (!isset($_GET['oauth_token']) && isset($_SESSION['state']) && $_SESSION['state'] == 1) {
+    $_SESSION['state'] = 0;
+}
+
+try {
+    $authType = ($_SESSION['state'] == 2) ? OAUTH_AUTH_TYPE_AUTHORIZATION : OAUTH_AUTH_TYPE_URI;
+    $oauthClient = new OAuth($consumerKey, $consumerSecret, OAUTH_SIG_METHOD_HMACSHA1, $authType);
+    $oauthClient->enableDebug();
+
+    if (!isset($_GET['oauth_token']) && !$_SESSION['state']) {
+        error_log("Fetching request token");
+        $requestToken = $oauthClient->getRequestToken($requestTokenURL, $callbackUrl);
+        $url = $authorizeURL . '?oauth_token=' . $requestToken['oauth_token'];
+        $_SESSION['secret'] = $requestToken['oauth_token_secret'];
+        $_SESSION['state'] = 1;
+        error_log("Redirect");
+        header('Location: ' . $url);
+        exit;
+    } else if ($_SESSION['state'] == 1) {
+        error_log("Fetching access token");
+        $oauthClient->setToken($_GET['oauth_token'], $_SESSION['secret']);
+        $accessToken = $oauthClient->getAccessToken($accessTokenURL);
+        $_SESSION['state'] = 2;
+        $_SESSION['token'] = $accessToken['oauth_token'];
+        $_SESSION['secret'] = $accessToken['oauth_token_secret'];
+        header('Location: ' . $callbackUrl);
+        exit;
+    } else {
+        error_log("Sending request to magento api");
+        $oauthClient->setToken($_SESSION['token'], $_SESSION['secret']);
+        $resourceUrl = "$apiUrl/orders";
+        $oauthClient->fetch($resourceUrl, array(), 'GET', array('Content-Type' => 'application/json'));
+        $productsList = json_decode($oauthClient->getLastResponse());
+        print_r($productsList);
+        echo "<p><a href=\"?reset_session=1\"> Reset session </a></p>";
+    }
+} catch (OAuthException $e) {
+    error_log("Oauth error: ".$e->getMessage());
+    print_r($e->getMessage());
+    echo "<br/>";
+    print_r($e->lastResponse);
+    
+    echo "<p><a href=\"?reset_session=1\"> Reset session </a></p>";
+}
